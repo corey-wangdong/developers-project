d344084887b9369372992d0762762b0e43e353b6
corey-wangdong <15379675451@163.com>
2025-01-19 14:46:57 +0800

    添加接口请求

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from 'axios'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState('我是默认数据')
+
+  const handleData = async () => {
+    const res = await axios.get('https://my-first-worker.15379675451.workers.dev/')
+    console.log(res)
+    setData(JSON.stringify(res.data))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>来啦老弟 --- 888</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleData}> 点击获取数据</button>
+      <p>{data}</p>
     </>
   )
 }
