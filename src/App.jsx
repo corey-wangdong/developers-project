76517406e83f4d504197366072769204a6aa8ca9
corey <15379675451@163.com>
2025-02-18 17:15:00 +0800

    dffds

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 
 
 function App() {
+  const [from, setFrom] = useState('')
   const [data, setData] = useState('我是默认数据')
 
   const handleData = async () => {
@@ -13,13 +14,18 @@ function App() {
   }
 
   const handlevercel = async () => {
-    const res = await axios.get('https://developers-project.vercel.app/api/test?name=123')
+    const res = await axios.get(`https://developers-project.vercel.app/api/test?name=${from || '未知来源'}`)
     console.log(res)
     setData(res.data.message || '')
   }
 
+  const handleBlur = (data) => {
+    setFrom(data)
+  }
+
   return (
     <>
+      <input type="text" placeholder='请输入来源' onBlur={handleBlur} />
       <button onClick={handlevercel}> 测试vercel</button>
       <button onClick={handleData}> 点击获取数据</button>
       <p>{data}</p>
