153a1437152354bb442a001716a030f526697c31
corey <15379675451@163.com>
2025-02-18 15:32:43 +0800

    测试

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,26 @@ function App() {
   const [data, setData] = useState('我是默认数据')
 
   const handleData = async () => {
-    const res = await axios.get('https://my-first-worker.15379675451.workers.dev/')
+    // const res = await axios.get('https://my-first-worker.15379675451.workers.dev/')
+    const res = await axios({
+      method: 'get',
+      url: 'http://localhost:8787',
+      // headers: {
+      //   'content-type': 'application/json',
+      //   'CF-Device-Type': 'mobile',
+      // },
+      params: {
+        a: 1,
+        b: 2,
+      }
+    })
     console.log(res)
     setData(JSON.stringify(res.data))
   }
 
   return (
     <>
+      <button onClick={handleData}> 测试vercel</button>
       <button onClick={handleData}> 点击获取数据</button>
       <p>{data}</p>
     </>
