b40065315c18fb99f38c703fe66a557132d751d7
corey <15379675451@163.com>
2025-02-18 15:35:21 +0800

    测试

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,7 @@ function App() {
   const [data, setData] = useState('我是默认数据')
 
   const handleData = async () => {
-    // const res = await axios.get('https://my-first-worker.15379675451.workers.dev/')
-    const res = await axios({
-      method: 'get',
-      url: 'http://localhost:8787',
-      // headers: {
-      //   'content-type': 'application/json',
-      //   'CF-Device-Type': 'mobile',
-      // },
-      params: {
-        a: 1,
-        b: 2,
-      }
-    })
+    const res = await axios.get('https://my-first-worker.15379675451.workers.dev/')
     console.log(res)
     setData(JSON.stringify(res.data))
   }
