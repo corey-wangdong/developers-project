785794927acce022d9b09e1d3c1181920b89dd88
corey <15379675451@163.com>
2025-02-18 17:08:20 +0800

    cdd

--- api/test.js
+++ api/test.js
@@ -1,5 +1,5 @@
 export default (req, res) => {
   console.log('req---', req);
   console.log('res---', res);
-  res.status(200).json({ message: 'Hello from Vercel Serverless!' });
+  res.status(200).json({ message: 'Hello from Vercel Serverless!' + JSON.stringify(req.query) });
 };
